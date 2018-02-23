df3457e9822808bcad12c60888aea96b13ca8aac
Andrzej Jasinski <andrzej.jasinski@fortedigital.no>
2018-02-23 21:56:45 +0100

    working mode change

--- src/device/Purifier.ts
+++ src/device/Purifier.ts
@@ -1,13 +1,57 @@
-import miio from 'miio'
+const miio = require('miio');
+
+export interface PurifierSettings
+{
+    ip: string
+}
 
 export default class Purifier
 {
+    private settings: PurifierSettings
+    constructor(settings: PurifierSettings){
+        this.settings = settings
+    }
     device: any
     async connect(){
-        this.device = miio.device({ address: '192.168.1.2' })
+        this.device = await miio.device({ address: this.settings.ip })
     }
 
     async on(){
         return this.device.setPower(true)
     }
+
+    async off(){
+        return this.device.setPower(false)
+    }
+
+    async powerStatus(){
+        return this.device.power()
+    }
+
+    async getTemperature(){
+        return this.device.temperature()
+    }
+
+    async getHumidity(){
+        return this.device.relativeHumidity()
+    }
+
+    async setFavourite()
+    {
+        return this.device.setMode('favorite')
+    }
+
+    async setSpeed(speed: number)
+    {
+        if(speed>16 || speed < 0) return Promise.reject('Speed must be between 0 and 16')
+
+        const currentMode = await this.device.mode()
+        if(currentMode !== 'favorite')
+        {
+            console.log(currentMode)
+            await this.setFavourite()
+        }
+        return this.device.favoriteLevel(speed)
+    }
+
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
-import miio from 'miio'
 import Purifier from './device/Purifier';
+require('dotenv').config()
 
-const device = new Purifier()
-device.connect().then(() => device.on())
+const settings = {
+    ip: process.env.DEVICE_IP
+}
+if(!settings.ip) {
+    console.log('No ip set')
+    process.exit(1)
+}
+const device = new Purifier(settings)
+device.connect().then(async () => {
+    const temperature = await  device.getTemperature()
+    const humid = await  device.getHumidity()
+    console.log(temperature)
+    console.log(humid)
+    await device.setSpeed(1)
+})
+.then(() => process.exit())
+.catch(err => {
+    console.log(err)
+    process.exit(1)
+})
