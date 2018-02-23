7846efc176a63aea4a80c2825634a5072bd76c03
Andrzej Jasinski <andrzej.jasinski@fortedigital.no>
2018-02-23 21:26:02 +0100

    somet changes

--- src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Purifier_1 = require("./device/Purifier");
-var device = new Purifier_1["default"]();
-device.connect().then(function () { return device.on(); });
